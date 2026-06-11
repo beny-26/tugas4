0a8cfa64994bd045341c81cf69e22e3cbaf4fa06
beny damanik <benydamanik24@gmail.com>
2026-06-11 19:44:05 +0700

    tugas4

---
+++ java.js
@@ -0,0 +1,22 @@
+const inputTugas = document.getElementById("tugas");
+const tombolTambah = document.getElementById("tambah");
+const daftarTugas = document.getElementById("daftarTugas");
+
+tombolTambah.addEventListener("click", function () {
+    if (inputTugas.value.trim() === "") {
+        alert("Tugas tidak boleh kosong!");
+        return;
+    }
+    const li = document.createElement("li");
+    li.textContent = inputTugas.value;
+    const tombolHapus = document.createElement("button");
+    tombolHapus.textContent = "Hapus";
+    tombolHapus.classList.add("hapus");
+
+    tombolHapus.addEventListener("click", function () {
+        li.remove();
+    });
+    li.appendChild(tombolHapus);
+    daftarTugas.appendChild(li);
+    inputTugas.value = "";
+});
